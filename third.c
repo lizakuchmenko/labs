164878355b154e9242c90b91bc9e84adf84ad302
Yelyzaveta Kuchmenko <32847274+lizakuchmenko@users.noreply.github.com>
2017-10-16 21:22:44 +0300

    improve third lab

--- third.c
+++ third.c
@@ -4,20 +4,20 @@
 int main(int argc, char **argv) {
   float y;
   float a = 1.8;
-  float x = -2.0;
-  float x1 = 2.0;
+  float x_min = -2.0;
+  float x_max = 2.0;
   float dx = 0.5;
 
-  for( ; x <= x1; x += dx) {
-    if(x >= 1) {
+  for (float x = x_min; x <= x_max; x += dx) {
+    if (x >= 1) {
       y = log(x);
     }
-    else if(x >= -1.0 && x <= 1.0) {
+    else if (x > -1.0 && x < 1.0) {
       y = sqrt(x * x + pow(a, 3.0));
     }
-    else if(x <=- 1.0) {
+    else {
       y = exp(x);
     }
-  printf("%f\t %f\n", x, y);
+    printf("%f\t %f\n", x, y);
   }
 }
